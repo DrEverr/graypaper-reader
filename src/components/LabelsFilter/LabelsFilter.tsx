5fde9f2b8c5d7d246b9a871b7a755c89aa3d0e87
DrEverr <ms1997sokol@gmail.com>
2025-03-07 10:48:52 +0100

    labels tree display

--- src/components/LabelsFilter/LabelsFilter.tsx
+++ src/components/LabelsFilter/LabelsFilter.tsx
@@ -1,42 +1,63 @@
 import "./LabelsFilter.css";
-import { type MouseEventHandler, useCallback } from "react";
-import { Label } from "../Label/Label";
-import type { ILabel } from "../NotesProvider/hooks/useLabels";
+import { type ILabel, Label, getFullLabelName } from "../Label/Label";
 
 export type LabelsFilterProps = {
   labels: ILabel[];
-  onToggleLabel: (label: string) => void;
+  onToggleLabel: (label: ILabel) => void;
 };
 
-export function LabelsFilter({ labels, onToggleLabel }: LabelsFilterProps) {
+export function LabelsFilterTree({ labels, onToggleLabel }: LabelsFilterProps) {
+  console.log("LabelsFilterTree", labels);
   return (
-    <div className="labels filter">
-      {labels.map((label) => (
-        <LabelLink key={label.label} label={label} onToggleLabel={onToggleLabel} />
-      ))}
+    <div className="label-tree-content">
+      {labels
+        .filter((label) => label.parent === undefined) // only root labels
+        .map((label) => (
+          <LabelNode key={label.label} labels={labels} label={label} onToggleLabel={onToggleLabel} />
+        ))}
     </div>
   );
 }
 
 type LabelLinkProps = {
+  labels: ILabel[];
   label: ILabel;
+  prefix?: string;
   onToggleLabel: LabelsFilterProps["onToggleLabel"];
 };
 
-function LabelLink({ label, onToggleLabel }: LabelLinkProps) {
-  const selectLabel = useCallback<MouseEventHandler>(
-    (e) => {
-      e.preventDefault();
-      onToggleLabel(label.label);
-    },
-    [label, onToggleLabel],
-  );
+function LabelNode({ labels, label, onToggleLabel }: LabelLinkProps) {
+  const isActive = label.isActive;
+  const children = labels.filter((l) => {
+    if (l.parent) {
+      return getFullLabelName(l.parent) === getFullLabelName(label);
+    }
+    false;
+  });
+  const hasChildren = children.length > 0;
+  const prefix = hasChildren ? (isActive ? "▼" : "▶") : (isActive ? "⊙" : "∅");
+  const clazz = `label-link ${isActive ? "active" : ""}`;
 
-  const clazz = `label-link ${label.isActive ? "active" : ""}`;
-  const ico = label.isActive ? "⊙" : "∅";
   return (
-    <a href="#" className={clazz} onClick={selectLabel}>
-      <Label label={label.label} prefix={ico} />
-    </a>
+    <div className="label-node">
+      <div
+        className="label-node-header"
+        onClick={() => onToggleLabel(label)}
+        onKeyUp={(e) => e.key === "Enter" && onToggleLabel(label)}
+        tabIndex={0}
+        role="button"
+      >
+        <div className={clazz}>
+          <Label key={getFullLabelName(label)} label={label} prefix={prefix} />
+        </div>
+      </div>
+      {isActive && hasChildren && (
+        <div className="label-node-content">
+          {children.map((child) => (
+            <LabelNode key={child.label} labels={labels} label={child} onToggleLabel={onToggleLabel} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
